2e05c67ad62e9d971747dbaccc26b74d6309b937
ahmed-niaz <niazahmed.an@gmail.com>
2025-06-03 20:08:07 +0600

    folder added in .gitingnore

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -3,10 +3,26 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
 
+// create admin
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "admin created successfully",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.message || "Something went wrong",
+      error: err,
+    });
+  }
 };
+
+// get admin
+const getAdmin = async (req: Request, res: Response) => {};
 
 export const userController = {
   createAdmin,
