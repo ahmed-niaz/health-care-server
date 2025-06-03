5c3489439032e3f03f84643e0b6cd80f0e6c0983
ahmed-niaz <niazahmed.an@gmail.com>
2025-06-04 01:03:05 +0600

    optimize search

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 
 const getAdmin = async (req: Request, res: Response) => {
+  //   console.log("from the postman search or filter query", req.query);
   try {
-    const result = await adminService.getAdmin();
+    const result = await adminService.getAdmin(req.query);
     res.status(200).json({
       success: true,
       message: "admin data retrieve successfully",
--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,9 +1,38 @@
-import { PrismaClient } from "../../../generated/prisma";
+import { Prisma, PrismaClient } from "../../../generated/prisma";
 
 const prisma = new PrismaClient();
 
-const getAdmin = async () => {
-  return await prisma.admin.findMany();
+const getAdmin = async (params: any) => {
+  //   console.log({ params });
+
+  const andConditions: Prisma.AdminWhereInput[] = [];
+
+  if (params.searchTerm) {
+    andConditions.push({
+      OR: [
+        {
+          name: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: params.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    });
+  }
+
+  const whereAsObject: Prisma.AdminWhereInput = { AND: andConditions };
+
+  console.dir(andConditions, { depth: "infinity" });
+
+  return await prisma.admin.findMany({
+    where: whereAsObject,
+  });
 };
 
 export const adminService = { getAdmin };
