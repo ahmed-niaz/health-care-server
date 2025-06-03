bcf9a09b83ca97ca796183189f7e3df35595033d
ahmed-niaz <niazahmed.an@gmail.com>
2025-06-03 20:04:21 +0600

    remove test folder

--- src/app/modules/test/test.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { testController } from "./test.controller";
-
-const router = express.Router();
-
-router.post("/create-user", testController.testCreateUser);
-
-export const testRoutes = router;
