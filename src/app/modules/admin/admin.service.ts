5cad2f2a78ccc2b105487efd7c388551a0fe2671
ahmed-niaz <niazahmed.an@gmail.com>
2025-06-13 10:50:05 +0600

    / TODO: Apply search or filter logic on specific fields based on provided filterData;

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,14 +1,25 @@
+import { equal } from "assert";
 import { Prisma, PrismaClient } from "../../../generated/prisma";
 
 const prisma = new PrismaClient();
 
 const getAdmin = async (params: any) => {
-  //   console.log({ params });
+  // ! search on the specific fields.
 
+  const { searchTerm, ...filterData } = params;
   const andConditions: Prisma.AdminWhereInput[] = [];
   const adminSearchableFields = ["name", "email"];
 
   /*
+
+  console.log({ params });
+  console.log({ searchTerm });
+  console.log({ filterData });
+
+  const keys = Object.keys(filterData).length;
+  console.log({ keys });
+
+
 [
         {
           name: {
@@ -23,8 +34,10 @@ const getAdmin = async (params: any) => {
           },
         },
       ],
+
 */
 
+  // TODO: search optimization
   if (params.searchTerm) {
     andConditions.push({
       // array & using loop [map: because map return an array]
@@ -40,6 +53,21 @@ const getAdmin = async (params: any) => {
   const whereAsObject: Prisma.AdminWhereInput = { AND: andConditions };
 
   // console.dir(andConditions, { depth: "infinity" });
+
+  // TODO: // TODO: Apply search or filter logic on specific fields based on provided filterData
+
+  if (Object.keys(filterData).length > 0) {
+    // TODO: though it is an object so convert into an array
+
+    andConditions.push({
+      AND: Object.keys(filterData).map((field) => ({
+        [field]: {
+          //! exact match
+          equals: filterData[field],
+        },
+      })),
+    });
+  }
 
   return await prisma.admin.findMany({
     where: whereAsObject,
